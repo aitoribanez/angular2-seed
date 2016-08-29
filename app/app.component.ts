8a6d3f5ed38765939ef6b4b3b1c2a307bf5fe721
Aitor Ibañez <freelance@aitoribanez.com>
2016-08-29 21:15:36 +0200

    getting result from openweather on service. need promise to run ok

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from "@angular/core";
-import { weather } from "../node_modules/openweather-apis/index.js";
-import { CONFIG } from "../config";
 import { WeatherService } from "./app.service";
 
 @Component({
@@ -10,20 +8,8 @@ import { WeatherService } from "./app.service";
 })
 
 export class AppComponent {
-  private _key = CONFIG.OPEN_WEATHER_KEY;
-  private _latitude = 50.0467656;
-  private _longitude = 20.0048731;
-
   constructor(private _weatherInfo: WeatherService) {
-
+    console.log("appc");
     console.log(this._weatherInfo.getWeather());
-
-    weather.setLang("es");
-    weather.setCoordinate(this._latitude, this._longitude);
-    weather.setAPPID(this._key);
-    weather.getAllWeather(function(err, res){
-      if (err) { console.log(err); }
-      console.log(res);
-    });
   }
 }
--- app/app.service.ts
+++ app/app.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from "@angular/core";
+import { weather as weatherApi} from "../node_modules/openweather-apis/index.js";
+import { CONFIG } from "../config";
 
 @Injectable()
 export class WeatherService {
-    getWeather = () => [
-        { id: 1, name: "paco" },
-        { id: 2, name: "ana" },
-        { id: 3, name: "maira" }
-    ];
+  // constructor() {}
+
+  public getWeather = () => {
+    weatherApi.setLang("es");
+    weatherApi.setCoordinate(50.0467656, 20.0048731);
+    weatherApi.setAPPID(CONFIG.OPEN_WEATHER_KEY);
+    weatherApi.getAllWeather(function(err, res){
+      if (err) { console.log(err); }
+      console.log("res");
+      console.log(res);
+    });
+  }
 }
