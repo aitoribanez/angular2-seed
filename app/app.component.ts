6c295348897599b4848992751b7caa52cab0c699
Aitor Ibañez <freelance@aitoribanez.com>
2016-09-04 02:58:11 +0200

    needed properties added to component

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,19 +4,44 @@ import { WeatherService } from './app.service'
 @Component({
   selector: 'my-weather',
   template: `Humedad: {{ _humidity }}
-    Presión {{ _pressure }}`,
+    Presión {{ _pressure }}
+    Temperatura {{ _temp }}
+    Temperatura mínima {{ _minTemp }}
+    Temperatura máxima {{ _maxTemp }}
+    Puesta de sol {{ _sunrise }}
+    Amanecer {{ _sunset }}
+    Icono {{ _icon }}
+    Nombre {{ _name }}
+    `,
   providers: [ WeatherService ]
 })
 
 export class AppComponent {
   _humidity: number
-  _pressure: number = 123
+  _pressure: number
+  _temp: number
+  _minTemp: number
+  _maxTemp: number
+  _description: string
+  _sunrise: number
+  _sunset: number
+  _icon: string
+  _name: string
 
   constructor(private _weatherInfo: WeatherService) {
     let _weather = this._weatherInfo.getWeather()
     _weather.then((res) => {
       console.log(res)
       this._humidity = res['main']['humidity']
+      this._pressure = res['main']['pressure']
+      this._temp = res['main']['temp']
+      this._minTemp = res['main']['temp_min']
+      this._maxTemp = res['main']['temp_max']
+      this._description = res['weather'][0]['description']
+      this._icon = res['weather'][0]['icon']
+      this._sunrise = res['sys']['sunrise']
+      this._sunset = res['sys']['sunset']
+      this._name = res['name']
     })
   }
 }
