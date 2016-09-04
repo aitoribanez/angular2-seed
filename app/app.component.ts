2120d66ad9b44265b82b6ead67da1b35c3733f78
Aitor Ibañez <freelance@aitoribanez.com>
2016-09-04 02:25:10 +0200

    add example properties to weatherComponent

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,16 +3,20 @@ import { WeatherService } from './app.service'
 
 @Component({
   selector: 'my-weather',
-  template: 'Open weather example',
+  template: `Humedad: {{ _humidity }}
+    Presión {{ _pressure }}`,
   providers: [ WeatherService ]
 })
 
 export class AppComponent {
+  _humidity: number
+  _pressure: number = 123
+
   constructor(private _weatherInfo: WeatherService) {
     let _weather = this._weatherInfo.getWeather()
-
-    _weather.then(function(res) {
+    _weather.then((res) => {
       console.log(res)
+      this._humidity = res['main']['humidity']
     })
   }
 }
